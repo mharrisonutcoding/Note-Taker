5241d51c74ad76dd2856721e3d1072e27e1787e5
Mike Harrison <mharrisonutcoding@gmail.com>
2023-02-09 01:23:33 -0600

    added files to route folder for and server.js file

---
+++ Develop/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index.js");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Middleware that parses Json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+
+app.use(express.static("public"));
+
+// home route GET
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname), "./public/index.html")
+);
+// notes route GET
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
+
+// easy access to port server in terminal once its run
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} !`)
+);
